9846a3dfdd4cf39663b467bf2f077e06d8f1b7aa
laingoc99 <laingoc99@gmail.com>
2024-04-16 23:31:02 +0700

    update 164

--- app.py
+++ app.py
@@ -8,16 +8,19 @@ def load_config(filename='configemail.json'):
     try:
         with open(filename, 'r') as json_file:
             config_data = json.load(json_file)
-            email_info = config_data.get('email', {})
-            user_email = email_info.get('username', '')
-            user_password = email_info.get('password', '')
-            return user_email, user_password
+            emails_info = config_data.get('email', [])
+            user_emails = []
+            user_passwords = []
+            for email_info in emails_info:
+                user_emails.append(email_info.get('username', ''))
+                user_passwords.append(email_info.get('password', ''))
+            return user_emails, user_passwords
     except FileNotFoundError:
         print(f"File {filename} not found.")
-        return None, None
+        return [], []
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON in file {filename}: {e}")
-        return None, None
+        return [], []
 
 def load_text_file(filename):
     try:
@@ -29,15 +32,19 @@ def load_text_file(filename):
 
 def load_email_data():
     try:
-        recipient = load_text_file('receive.txt')
+        with open('receive.txt', 'r') as file:
+            recipients = [line.strip() for line in file if line.strip()] 
+            if not recipients:
+                raise ValueError("No recipients found in receive.txt")
+            
         sub_data = load_text_file('sub.txt').split(',')
         subject = sub_data[0].strip()
         content = sub_data[1].strip()
 
-        return recipient, subject, content
+        return recipients, subject, content
     except Exception as e:
         print(f"Error loading email data: {str(e)}")
-        return "", "", ""
+        return [], "", ""
 
 @app.route('/', methods=['GET'])
 def index():
@@ -45,14 +52,17 @@ def index():
 
 @app.route('/automation', methods=['POST'])
 def automation():
-    user_email, user_password = load_config()
-    recipient, subject, content = load_email_data()
+    user_emails, user_passwords = load_config()
+    recipients, subject, content = load_email_data()
 
     try:
-        send_email_diy(user_email, user_password, recipient, content, subject)
-        return jsonify({"message": "Email sent successfully!"}), 200
+        for recipient in recipients:
+            for user_email, user_password in zip(user_emails, user_passwords):
+                send_email_diy(user_email, user_password, recipient, content, subject)
+        return jsonify({"message": "Emails sent successfully!"}), 200
     except Exception as e:
         return jsonify({"error": f"Error sending email: {str(e)}"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
